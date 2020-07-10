008ed964706cfe8d36dc8914515923aa429282a4
IBricchi <ignacio.bricchi@gmail.com>
2020-07-11 00:54:46 +0200

    Add basic movement to player

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Movement : MonoBehaviour
+{
+	public Rigidbody2D rb;
+	public float speed = 200f;
+
+	private Vector2 dir = Vector2.zero;
+
+	public void FixedUpdate()
+	{
+		dir = Vector2.zero;
+
+		if (Input.GetKey(KeyCode.W)) dir.y += 1;
+		if (Input.GetKey(KeyCode.A)) dir.x -= 1;
+		if (Input.GetKey(KeyCode.S)) dir.y -= 1;
+		if (Input.GetKey(KeyCode.D)) dir.x += 1;
+
+		dir = Vector2.ClampMagnitude(dir, 1);
+
+		rb.velocity = dir * speed * Time.fixedDeltaTime;
+	}
+}
