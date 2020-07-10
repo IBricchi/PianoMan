00595e99f515dd1c3ca08803004c0cd73dfcc101
IBricchi <ignacio.bricchi@gmail.com>
2020-07-11 00:02:05 +0200

    Setup basic scenes to manage game, with simple fade transitions

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader: MonoBehaviour
+{
+	public Animator transition;
+	public float tranistionTime = 1f;
+
+	public void LoadLevel(int levelIndex)
+	{
+		StartCoroutine(LoadLevelCR(levelIndex));
+	}
+
+	public void LoadNextLevel()
+	{
+		StartCoroutine(LoadLevelCR(SceneManager.GetActiveScene().buildIndex + 1));
+	}
+
+	IEnumerator LoadLevelCR(int levelIndex)
+	{
+		transition.SetTrigger("Start");
+
+		yield return new WaitForSeconds(tranistionTime);
+
+		SceneManager.LoadScene(levelIndex);
+	}
+
+}
