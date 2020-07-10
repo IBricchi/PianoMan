0009ad95c67c7e961b04e5dd7f26c0d0e72fdad8
IBricchi <ignacio.bricchi@gmail.com>
2020-07-11 00:11:35 +0200

    Add simple play button to menu

---
+++ Assets/Scripts/StartMenu/StartButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class StartButton: MonoBehaviour
+{
+	public LevelLoader ll;
+	Button start;
+
+	private void Start()
+	{
+		start = gameObject.GetComponent<Button>();
+		start.onClick.AddListener(() => ll.LoadLevel(2)); 
+	}
+}
