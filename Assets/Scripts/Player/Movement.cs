c240e361bd20790d28492711c1f9db29c40d08e8
IBricchi <ignacio.bricchi@gmail.com>
2020-07-11 12:30:40 +0200

    Add simple animations to player

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
 public class Movement : MonoBehaviour
 {
 	public Rigidbody2D rb;
+	public Animator anim;
 	public float speed = 200f;
 
 	private Vector2 dir = Vector2.zero;
+	private bool lastRight = true;
+
+	private bool isSitting = true;
+	private bool isIdleStanding = false;
+	private float idleStandingTime = 0f;
+
 
 	public void FixedUpdate()
 	{
@@ -22,5 +30,34 @@ public class Movement : MonoBehaviour
 		dir = Vector2.ClampMagnitude(dir, 1);
 
 		rb.velocity = dir * speed * Time.fixedDeltaTime;
+
+		anim.SetFloat("speed", rb.velocity.sqrMagnitude);
+
+		if (dir.x != 0) lastRight = dir.x > 0;
+		anim.SetBool("right", lastRight);
+
+		if(anim.GetFloat("speed") < 0.1 && !isSitting && !isIdleStanding)
+		{
+			isIdleStanding = true;
+			idleStandingTime = 0f;
+		}
+
+		if(isIdleStanding)
+		{
+			idleStandingTime += Time.fixedDeltaTime;
+			if(idleStandingTime > 1f)
+			{
+				anim.SetTrigger("sit");
+				isIdleStanding = false;
+				isSitting = true;
+			}
+		}
+		
+		if((isSitting || isIdleStanding) && anim.GetFloat("speed") > 0.1)
+		{
+			isIdleStanding = false;
+			isSitting = false;
+		}
+
 	}
 }
